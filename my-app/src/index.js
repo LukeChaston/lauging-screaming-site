6d83ec71d49b5616565c3951fb8f4a6217a7c823
lukechaston@gmail.com <lukechaston@Lukes-MacBook-Pro.local>
2020-01-13 16:27:15 +0000

    added laughing and screaming options to the navbar.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import laughing from "./laughing";
+import screaming from "./screaming";
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
@@ -15,16 +17,20 @@ class routing extends React.Component {
             <li>
                 <Link to="/">Blank</Link>
             </li>
+
             <li>
                  <Link to="/">Laughing</Link>
             </li>
+
             <li>
                 <Link to="/">Screaming</Link>
             </li>
+
                </ul>
-            <Route exact path='/' component={ App }/>
-        <Route path='/accounts' component={ accounts }/>
-        <Route path='/submission' component={ submission }/>
+            <Switch>
+                    <Route exact path='/' component={ App }/>
+                     <Route path='/laughing' component={ laughing }/>
+                   <Route path='/screaming' component={ screaming }/>
         </Switch>
             </div>
             </Router>
