ecbb33ea69517efaed8877be4f03cc12bf15341a
lukechaston@gmail.com <lukechaston@Lukes-MacBook-Pro.local>
2020-01-13 15:58:15 +0000

    created template for nav bar. Doesn't work yet and looks like a mess though.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+
+
+class routing extends React.Component {
+    render() {
+        return(
+            <Router>
+            <div name="nav">
+            <ul>
+            <li>
+                <Link to="/">Blank</Link>
+            </li>
+            <li>
+                 <Link to="/">Laughing</Link>
+            </li>
+            <li>
+                <Link to="/">Screaming</Link>
+            </li>
+               </ul>
+            <Route exact path='/' component={ App }/>
+        <Route path='/accounts' component={ accounts }/>
+        <Route path='/submission' component={ submission }/>
+        </Switch>
+            </div>
+            </Router>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
