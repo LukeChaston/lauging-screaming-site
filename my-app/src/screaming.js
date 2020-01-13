29af612abc8a7920ae923ce5f2441fb5db8fb138
lukechaston@gmail.com <lukechaston@Lukes-MacBook-Pro.local>
2020-01-13 16:04:05 +0000

    added page for screaming gifs.

---
+++ my-app/src/screaming.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class screaming extends React.Component {
+    render() {
+        return(
+            <div className="screaming">
+
+            </div>
+    )
+    }
+}
+
+export default screaming
+
+ReactDOM.render(<submission />,document.getElementById('root'));
