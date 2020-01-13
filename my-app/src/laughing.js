21ca70912034dfd2dbe0d0fad09f1cb1dbe30f39
lukechaston@gmail.com <lukechaston@Lukes-MacBook-Pro.local>
2020-01-13 16:03:42 +0000

    added page for laughing gifs and images.

---
+++ my-app/src/laughing.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class laughing extends React.Component {
+    render() {
+        return(
+            <div className="laughing">
+
+        </div>
+    )
+    }
+}
+
+export default laughing
+
+ReactDOM.render(<submission />,document.getElementById('root'));
